516ea7cd78753d61825eddec6b305a046c55f4da
MrNano08 <imrnano08@gmail.com>
2025-03-31 15:30:11 -0600

    Added local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import './App.css';
 
-import './App.css'
-
-interface Todo{
-   
-  description:string
+interface Todo {
+  description: string;
 }
 
 function App() {
+  const [todoDescription, setTodoDescription] = useState('');
+  const [todoList, setTodoList] = useState<Todo[]>(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
 
- const[todoDescription,setTodoDescription]= useState('')
- const [todoList, setTodoList] = useState<Todo[]>([])
- 
- const handleChange = (e: any) => {
-  setTodoDescription (e.target.value)
- }
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todoList));
+  }, [todoList]);
 
- const handleClik = () => {
- const tempTodoList = [... todoList]
- const newTodo = {
-  description: todoDescription
- }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoDescription(e.target.value);
+  };
 
- tempTodoList.unshift(newTodo)
+  const handleClick = () => {
+    if (!todoDescription.trim()) return;
+    const newTodo: Todo = {
+      description: todoDescription
+    };
+    setTodoList(prevList => [...prevList, newTodo]);
+    setTodoDescription('');
+  };
 
- setTodoList(tempTodoList)
-
- }
-
- return (
-    <>
-    <div style={{ border: '1px solid red',padding:10 }} >
-
-      <div> 
-        
+  return (
+    <div style={{ border: '1px solid red', padding: 10 }}>
+      <div>
         <input
-        type='text'
-        value={todoDescription}
-        onChange={handleChange}
-         style={{marginRight: 10}}/>
-        <button onClick={handleClik}>Add Item</button>
+          type='text'
+          value={todoDescription}
+          onChange={handleChange}
+          style={{ marginRight: 10 }}
+        />
+        <button onClick={handleClick}>Add Item</button>
       </div>
 
-      <div>TODOs Here: </div>
+      <div>TODOs Here:</div>
       <ul>
-        {todoList.map((todo,index) => {
-          return <li key = {index}>
-                 <input type='checkbox' />
-                 {todo.description} 
-             </li>
-        })}
+        {todoList.map((todo, index) => (
+          <li key={index} style={{ color: 'white' }}>
+            {todo.description}
+          </li>
+        ))}
       </ul>
-
-      </div>
-      </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
